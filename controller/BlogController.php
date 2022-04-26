1578d585b40e50966dfeb89e562dddbbb0bfd404
El houssaïne El jadid <ikky57@gmail.com>
2022-04-26 09:29:04 +0200

    mise ajour

--- controller/BlogController.php
+++ controller/BlogController.php
@@ -5,25 +5,30 @@ require 'model/ArticleManager.php';
 
 class BlogController
 {
+    private ArticleManager $articleManager;
 
+    public function __construct()
+    {
+        $this->articleManager = new ArticleManager();
+    }
     public function index()
     {
-        $articleManager = new ArticleManager();
-        $articles = $articleManager->getAll();
+
+        $articles = $this->articleManager->getAll();
         require 'view/blog/index.html.php';
     }
 
 
     public function article()
     {
-        $articleManager = new ArticleManager();
-        $article = $articleManager->getById($_GET['id']);
+
+        $article = $this->articleManager->getById($_GET['id']);
         require 'view/blog/article.html.php';
     }
     public function newArticle()
     {
-        $articleManager = new ArticleManager();
-        $article = $articleManager->addArticle($_POST['title'], $_POST['content']);
+
+        $article = $this->articleManager->addArticle($_POST['title'], $_POST['content']);
         header('Location: /?controller=blog');
     }
     public function showform()
@@ -34,20 +39,20 @@ class BlogController
     public function modifie()
 
     {
-        $articleManager = new ArticleManager();
-        $article = $articleManager->getById($_GET['id']);
+
+        $article = $this->articleManager->getById($_GET['id']);
         require 'view/blog/updatearticle.html.php';
     }
     public function insertmodife()
     {
-        $articleManager = new ArticleManager();
-        $article = $articleManager->updateArticle($_POST['id'], $_POST['title'], $_POST['content']);
+
+        $article = $this->articleManager->updateArticle($_POST['id'], $_POST['title'], $_POST['content']);
         header('Location: /?controller=blog');
     }
     public function delete()
     {
-        $articleManager = new ArticleManager();
-        $article = $articleManager->deleteArticle($_GET['id']);
+
+        $article = $this->articleManager->deleteArticle($_GET['id']);
         header('Location: /?controller=blog');
     }
 }
